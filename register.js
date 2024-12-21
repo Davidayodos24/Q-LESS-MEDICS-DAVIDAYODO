1680181e6cb7806d4dc5bc932fda1b8271cd980f
Ayodo David <160709923+Davidayodos24@users.noreply.github.com>
2024-12-21 22:40:52 +0300

    initial commit

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registerForm");
 
     form.addEventListener("submit", (event) => {
